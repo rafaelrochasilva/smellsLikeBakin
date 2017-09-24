f0a5dcd7793d6acd86c9e8895d66d3b8681eb06e
Rafael Rocha <rafaelrochasilva@gmail.com>
2017-09-24 19:55:33 -0300

    Fix identation and remove js from html

---
+++ js/contact.js
@@ -0,0 +1,38 @@
+var $submit = $(".submit input");
+var $required = $(".required");
+
+function containsBlanks(){
+  var blanks = $required.map(function(){ return $(this).val() == "";});
+  return $.inArray(true,blanks) != -1;
+}
+
+function isValidEmail(email){
+  return email.indexOf("@") != -1;
+}
+
+function requiredFilledIn(){
+  if(containsBlanks() || !isValidEmail($("#email").val())){
+    $submit.attr("disable", "disabled");
+  }else
+  {
+    $submit.removeAttr("disable");
+  }
+}
+
+$("#form span").hide()0;
+$("input, textarea").focus(function(){
+  $(this).next().fadeIn("slow");
+}).blur(function(){
+  $(this).next().fadeOut("slow");
+}).keyup(function(){
+  //Check all required fields.
+});
+
+$("#email").keyup(function(){
+  if(isValidEmail($(this).val()))
+    $(this).next().removeClass("error").addClass("valid");
+  else
+    $(this).next().removeClass("valid").addClass("error");
+});
+
+requiredFilledIn();
