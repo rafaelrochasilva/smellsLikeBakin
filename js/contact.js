d42e036425bfefcd13c16a0cf9f3102c88bfe934
Rafael Rocha <rafaelrochasilva@gmail.com>
2017-10-03 13:27:28 -0300

    Refactory Js files
    - Use cdn instead of store thirdparty files.
    - Remove the javascrit inside the html document.
    - Refactory javascript to be like AMD style.
    - Separete the Js files into contexts.
    - Create a new way to validate email form.
    - Add regex to validate de email form field.

--- js/contact.js
+++ js/contact.js
@@ -1,38 +1,68 @@
-var $submit = $(".submit input");
-var $required = $(".required");
+var Bakin = window.Bakin || {};
 
-function containsBlanks(){
-  var blanks = $required.map(function(){ return $(this).val() == "";});
-  return $.inArray(true,blanks) != -1;
-}
+Bakin.Contact = (function() {
+  var REGEX_TYPES = {
+    'email': /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    'text': /^[a-zA-Z ]{10,500}$/,
+    'input': /^[a-zA-Z ]{3,30}$/
+  };
 
-function isValidEmail(email){
-  return email.indexOf("@") != -1;
-}
+  function Contact() {
+    this.$input = $('[data-input]');
+    this.$submit = $('[data-btn-submit]');
 
-function requiredFilledIn(){
-  if(containsBlanks() || !isValidEmail($("#email").val())){
-    $submit.attr("disable", "disabled");
-  }else
-  {
-    $submit.removeAttr("disable");
+    this.addEventListeners();
   }
-}
 
-$(".mail-form span").hide();
-$("input, textarea").focus(function(){
-  $(this).next().fadeIn("slow");
-}).blur(function(){
-  $(this).next().fadeOut("slow");
-}).keyup(function(){
-  //Check all required fields.
-});
+  var fn = Contact.prototype;
 
-$("#email").keyup(function(){
-  if(isValidEmail($(this).val()))
-    $(this).next().removeClass("error").addClass("valid");
-  else
-    $(this).next().removeClass("valid").addClass("error");
-});
+  fn.addEventListeners = function() {
+    this.$input.on('keyup', this._verifyInput.bind(this));
+  };
 
-requiredFilledIn();
+  fn._verifyInput = function(e) {
+    var inputTarget = $(e.currentTarget),
+        message = inputTarget.val(),
+        inputContainer = inputTarget.parent(),
+        errorMessage = inputContainer.find('[data-input-type]'),
+        inputType = errorMessage.data('input-type');
+
+    if (this._isValid(message, inputType)) {
+      $(inputContainer).attr('data-input-valid', true);
+      errorMessage.hide();
+    } else {
+      $(inputContainer).data('data-input-valid', false);
+      errorMessage.show();
+    }
+
+    this._toggleSubmitButton();
+  };
+
+  fn._toggleSubmitButton = function() {
+    if(this._allFieldsValid()) {
+      this.$submit.removeAttr("disabled");
+    } else {
+      this.$submit.attr("disabled", true);
+    }
+  };
+
+  fn._allFieldsValid = function() {
+    var all_fields = [];
+
+    $('[data-input-valid]').each(function() {
+      all_fields.push($(this).attr('data-input-valid'));
+    });
+
+    return all_fields.every(function(element) { return element == "true" });
+  };
+
+  fn._isValid = function(message, inputType) {
+    var regex = REGEX_TYPES[inputType];
+
+    return regex.test(message);
+  };
+
+  return Contact;
+}());
+
+new Bakin.Contact();
